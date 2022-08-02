cafa6082fd16239f418e1e0b29e10e1a1c476f3f
olegcharkov <101756070+olegcharkov@users.noreply.github.com>
2022-08-02 12:03:27 +0300

    Update parser_2.py

--- Pet-Projects/parser_2.py
+++ Pet-Projects/parser_2.py
@@ -1,34 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import json
 from time import sleep
 import colorama
 
-# data_dict = []
-url = 'https://rentride.ru/arendovat/moskva/?min_year=2010&max_year=2022&page=1'
-html_text = requests.get(url).text
-soup = BeautifulSoup(html_text, 'lxml')
-infos = soup.find_all('div', class_='vehicle-card-vertical-body car-info-body')
-max_pages = 23
-# n = 1
+data_dict = []
+url = 'https://rentride.ru/arendovat/moskva/'
+n = 1
+params = {'page': 1}
 
-# map = {}
-# id = 0
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+last_page_num = int(soup.find_all('a', class_='pagination-number-item')[-1].text)
 
-for p in range(max_pages):
+for page_num in range(1, last_page_num-1):
     sleep(1)
-    cur_url = url + str(p+1)
-    print(colorama.Fore.YELLOW + "Скрапинг страницы #: %d" % (p+1) + colorama.Fore.WHITE)
+    params['page'] = page_num
+    cur_url = url + str(page_num + 1)
+    print(colorama.Fore.YELLOW + "Скрапинг страницы #: %d" % (page_num) + colorama.Fore.WHITE)
+    response = requests.get(url, params=params)
+    soup = BeautifulSoup(response.text, 'lxml')
+    infos = soup.find_all('div', class_='vehicle-card-vertical-body car-info-body')
 
-    for n, i in enumerate(infos, start=1):
+    for n, i in enumerate(infos, start=n):
         name = i.find('h4', class_='title-car').text.strip()
         stars = i.find('div', class_='rating-value').text.strip()
         comments = i.find('div', class_='car-review-count').text.strip()
         price = i.find('div', class_='car-price-info').text.strip()
         print(f'{n}: {name} цена:{price} с оценкой {stars} и количеством отзывов:{comments}')
 
-        # page_nav = soup.find_all('a', href='https://rentride.ru/arendovat/moskva/?min_year=2010&max_year=2022&page=23')
-        # if(len(page_nav)==0):
-        #     print(colorama.Fore.LIGHTRED_EX + 'Max_page')
-        #     break
+        data = {
+            'car': name,
+            'price': price,
+            'stars': stars,
+            'comments': comments,
+        }
 
+        data_dict.append(data)
+
+with open('data_2.json', 'w') as json_file:
+    json.dump(data_dict, json_file, indent=5)
